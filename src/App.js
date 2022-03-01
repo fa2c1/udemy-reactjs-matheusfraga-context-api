968bbf51830088cc7b0f29cad0b7bf597a1ccb70
Fernando Coutinho <fa2c1@hotmail.com>
2022-03-01 14:05:47 -0300

    Trabalhando sem Context Api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useState } from 'react';
+import Alunos from './components/Alunos';
+
 function App() {
+
+  const [nomeAluno, setNomeAluno] = useState('Fernando Coutinho');
+
   return (
     <div>
+      <h1>ESCOLA</h1>
+      <hr/>
+      <Alunos nome={nomeAluno} mudaNome={setNomeAluno} />
     </div>
   );
 }
